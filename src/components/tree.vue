0d140863cb933ff15baf46186efbe4a67c97c755
liuziyang <gnayizuil@gmail.com>
2020-01-09 20:49:24 +0800

    利用组件的递归形成树

---
+++ src/components/tree.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <div v-for="(item, index) in list" :key="index">
+        <span @click="changeStatus(index)">{{item.name}}</span>
+        <tree v-if="show" :list="item.cList"></tree>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["list"],
+  name: "tree",
+  data() {
+    return {
+        show:false
+    };
+  },
+  methods: {
+      changeStatus(idx){
+          this.show = !this.show
+      }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+div {
+    text-align: left;
+    padding-left: 20px !important;
+}
+</style>
